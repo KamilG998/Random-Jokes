d555e149393cea3c5bf69207424765d67dc0ceaa
KamilG998 <123089871+KamilG998@users.noreply.github.com>
2023-06-14 08:05:31 +0200

    Functional Node

--- jokes_app/server.js
+++ jokes_app/server.js
@@ -1,10 +1,11 @@
 const http = require("http");
 
-const {getJokes,getJoke,getRandom}=require("./controllers/jokeController");
+const {getJokes,getJoke,getRandom}= require("./controllers/jokeController");
+const {serveStaticFile}=require("./util/staticServer");
 
 
 const PORT=8080;
-const API_CONTENT_TYPE={"Content-type:": "application/json"};
+const API_CONTENT_TYPE={"Content-type" : "application/json"};
 
 const server = http.createServer(
     async function(req,res){
@@ -14,14 +15,46 @@ const server = http.createServer(
             let jokes= await getJokes();
 
             if(jokes){
-                res.writeHead(200,API_CONTENT_TYPE)
+                res.writeHead(200, API_CONTENT_TYPE)
             }else{
-                res.writeHead(404,API_CONTENT_TYPE)
+                res.writeHead(404, API_CONTENT_TYPE)
                 jokes={message:"Jokes not found"};
             }
+
             res.end(JSON.stringify(jokes));
+        }else 
+        if  (req.url === "/api/jokes/random" && req.method === "GET"){
+             let joke= await getRandom();
+
+            if(joke){
+                res.writeHead(200, API_CONTENT_TYPE);
+
+            }else{
+                res.writeHead(404, API_CONTENT_TYPE);
+                joke = { message: "Joke not found" };
+            }
+       
+            res.end(JSON.stringify(joke));
+        }else
+        if(req.url.match(/\/api\/jokes\/([0-9]+)/)
+        && req.method==="GET"  ) {
+            const id = req.url.split("/")[3];
+
+            let joke = await getJoke(id);
+            if(joke){
+                res.writeHead(200,API_CONTENT_TYPE)
+            }else{
+                res.writeHead(404,API_CONTENT_TYPE);
+                joke={message:"Joke not found"}
+            }
+
+            res.end(JSON.stringify(joke));
+        } else{
+            serveStaticFile(req,res)
         }
-    }
+        
+    }          
+  
 );
 
 server.listen(PORT);
